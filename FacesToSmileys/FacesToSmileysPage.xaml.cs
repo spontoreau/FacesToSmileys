130dc8f32aaae9f0e6ace95d8862e9730744c4db
pontoreau <pontoreau.sylvain@gmail.com>
2016-12-14 23:00:49 +0100

    Add Xam.Plugin.Media and basic interface

--- FacesToSmileys/FacesToSmileysPage.xaml.cs
+++ FacesToSmileys/FacesToSmileysPage.xaml.cs
@@ -1,4 +1,6 @@
-﻿using Xamarin.Forms;
+﻿using System;
+using Plugin.Media;
+using Xamarin.Forms;
 
 namespace FacesToSmileys
 {
@@ -7,6 +9,35 @@ namespace FacesToSmileys
         public FacesToSmileysPage()
         {
             InitializeComponent();
+        }
+
+        async void Handle_Clicked(object sender, System.EventArgs e)
+        {
+            await CrossMedia.Current.Initialize();
+
+            if (!CrossMedia.Current.IsCameraAvailable || !CrossMedia.Current.IsTakePhotoSupported)
+            {
+                DisplayAlert("No Camera", ":( No camera available.", "OK");
+                return;
+            }
+
+            var file = await CrossMedia.Current.TakePhotoAsync(new Plugin.Media.Abstractions.StoreCameraMediaOptions
+            {
+                Directory = "Sample",
+                Name = "test.jpg"
+            });
+
+            if (file == null)
+                return;
+
+            await DisplayAlert("File Location", file.Path, "OK");
+
+            image.Source = ImageSource.FromStream(() =>
+            {
+                var stream = file.GetStream();
+                file.Dispose();
+                return stream;
+            });
         }
     }
 }
