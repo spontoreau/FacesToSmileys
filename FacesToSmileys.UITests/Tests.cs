f1140be968e9425802e6ca5910de1ae3a58d0273
Sebastien Bovo <sebastienbovo@outlook.com>
2017-01-24 09:09:22 +0100

    Test only for Android for now

--- FacesToSmileys.UITests/Tests.cs
+++ FacesToSmileys.UITests/Tests.cs
@@ -8,7 +8,7 @@ using Xamarin.UITest.Queries;
 namespace FacesToSmileys.UITests
 {
     [TestFixture(Platform.Android)]
-    [TestFixture(Platform.iOS)]
+    //[TestFixture(Platform.iOS)]
     public class Tests
     {
         IApp app;
