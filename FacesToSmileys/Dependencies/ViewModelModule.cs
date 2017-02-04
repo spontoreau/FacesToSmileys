e97b97b277130cd2511cb0e9fb43b39a595499b5
pontoreau <pontoreau.sylvain@gmail.com>
2017-02-04 13:17:08 +0100

    Add Autofac package and basic module

---
+++ FacesToSmileys/Dependencies/ViewModelModule.cs
@@ -0,0 +1,18 @@
+﻿using Autofac;
+using FacesToSmileys.ViewModels;
+
+namespace FacesToSmileys.Dependencies
+{
+    public class ViewModelModule : Module
+    {
+        /// <summary>
+        /// Load the module.
+        /// </summary>
+        /// <param name="builder">Builder.</param>
+        protected override void Load(Autofac.ContainerBuilder builder)
+        {
+            builder.RegisterType<TakePhotoViewModel>()
+                   .OnActivated(e => e.Context.InjectUnsetProperties(e.Instance));
+        }
+    }
+}
