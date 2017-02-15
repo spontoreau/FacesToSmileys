f677e977549643162a09fe9a40e0b09e86cf9460
Sylvain PONTOREAU <pontoreau.sylvain@gmail.com>
2017-02-15 23:04:41 +0100

    Switch orientation to Sensor landscape (#20)

--- FacesToSmileys.Droid/MainActivity.cs
+++ FacesToSmileys.Droid/MainActivity.cs
@@ -1,20 +1,13 @@
-﻿using System;
-
+﻿
 using Android.App;
-using Android.Content;
 using Android.Content.PM;
-using Android.Runtime;
-using Android.Views;
-using Android.Widget;
 using Android.OS;
-// Visual Studio Mobile Center
-using Microsoft.Azure.Mobile;
 using FacesToSmileys.Dependencies;
 using FacesToSmileys.Services;
 
 namespace FacesToSmileys.Droid
 {
-    [Activity(ScreenOrientation = ScreenOrientation.Landscape, MainLauncher = false, Label = "FacesToSmileys.Droid", Icon = "@drawable/icon", Theme = "@style/MyTheme", ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(ScreenOrientation = ScreenOrientation.SensorLandscape, MainLauncher = false, Label = "FacesToSmileys.Droid", Icon = "@drawable/icon", Theme = "@style/MyTheme", ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle bundle)
