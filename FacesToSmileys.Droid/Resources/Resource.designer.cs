298b18cf4057249758eac56786cade0ad4ff95e9
pontoreau <pontoreau.sylvain@gmail.com>
2017-03-06 21:13:46 +0100

    Add twitter image

--- FacesToSmileys.Droid/Resources/Resource.designer.cs
+++ FacesToSmileys.Droid/Resources/Resource.designer.cs
@@ -2201,14 +2201,17 @@ namespace FacesToSmileys.Droid
 			// aapt resource value: 0x7f0200a0
 			public const int mr_ic_play_light = 2130837664;
 			
-			// aapt resource value: 0x7f0200a3
-			public const int notification_template_icon_bg = 2130837667;
+			// aapt resource value: 0x7f0200a4
+			public const int notification_template_icon_bg = 2130837668;
 			
 			// aapt resource value: 0x7f0200a1
 			public const int photo = 2130837665;
 			
 			// aapt resource value: 0x7f0200a2
 			public const int splash = 2130837666;
+			
+			// aapt resource value: 0x7f0200a3
+			public const int twitter = 2130837667;
 			
 			static Drawable()
 			{
