1e45258ad45c46af1d13db2e88d5c9b1d36e9cf8
Vtek <pontoreau.sylvain@gmail.com>
2017-08-02 23:51:20 +0200

    Fix Android problem when register dependencies

--- FacesToSmileys.Droid/MainActivity.cs
+++ FacesToSmileys.Droid/MainActivity.cs
@@ -7,14 +7,14 @@ namespace FacesToSmileys.Droid
     [Activity(ScreenOrientation = ScreenOrientation.FullSensor, MainLauncher = false, Label = "FacesToSmileys.Droid", Icon = "@drawable/icon", Theme = "@style/MyTheme", ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
-        protected override void OnCreate(Bundle bundle)
+        protected override void OnCreate(Bundle savedInstanceState)
         {
             TabLayoutResource = Resource.Layout.Tabbar;
             ToolbarResource = Resource.Layout.Toolbar;
 
-            base.OnCreate(bundle);
+            base.OnCreate(savedInstanceState);
 
-            Xamarin.Forms.Forms.Init(this, bundle);
+            Xamarin.Forms.Forms.Init(this, savedInstanceState);
 
             LoadApplication(new App());
         }
